6fb1c69403902a14084e5ba958cca10dc1ec2d9b
Adriano Patrick Cunha <adrianopatrick@gmail.com>
2020-06-06 21:34:35 -0300

    primeiro serviço de consulta de cep

---
+++ src/resources/CEPResource.js
@@ -0,0 +1,36 @@
+const https = require('https');
+
+exports.get = async (req, res, next) => {
+
+  const response = await httpGet(req.params.cep);
+  res.status(200).send(`Encontrei a ${response[0].nome}, ela fica localizada na ${response[0].enderecoAps}, ela está aberta de ${response[0].horarioAtendimento}`);
+}
+
+function httpGet(cep) {
+  return new Promise(((resolve, reject) => {
+    var options = {
+        host: 'vip-pmeuinssprxr.inss.gov.br',
+        path: `/apis/localizadorApsServices/buscaCep/${cep}`,
+        method: 'GET',
+    };
+    
+    const request = https.request(options, (response) => {
+
+      response.setEncoding('utf8');
+      let returnData = '';
+
+      response.on('data', (chunk) => {
+        returnData += chunk;
+      });
+
+      response.on('end', () => {
+        resolve(JSON.parse(returnData));
+      });
+
+      response.on('error', (error) => {
+        reject(error);
+      });
+    });
+    request.end();
+  }));
+}
